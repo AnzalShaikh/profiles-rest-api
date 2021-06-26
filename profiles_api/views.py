321b3c6d79455949ecf9c83d12130322f093e6e4
EjazSk <ejaz.sk124@gmail.com>
2021-06-26 16:31:16 +0530

    Serializer & viewset for user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from profiles_api import serializers
+from profiles_api import models
 
-# Create your views here.
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
